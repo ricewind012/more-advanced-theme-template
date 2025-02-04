b9fe354c64951a54bb3e18425fa5444ec3db6725
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2025-02-04 19:11:20 +0000

    fix not working on windows

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 import fs from "node:fs";
+import yargs from "yargs";
 import postcssFunctions from "postcss-functions";
 import postcssSass from "@csstools/postcss-sass";
 import { selectorReplacerPlugin } from "steam-theming-utils/postcss-plugin";
+
+const { argv } = yargs(process.argv);
+
+// Recreate the CSS output directory
+fs.rmSync(argv.dir, { recursive: true, force: true });
+fs.mkdirSync(argv.dir);
 
 const result = fs.readFileSync("steam_version").toString();
 const getSteamVersion = () => `"${result}"`;
